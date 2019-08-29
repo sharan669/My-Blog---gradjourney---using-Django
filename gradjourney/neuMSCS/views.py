0338f486f048196aef19d3c1da172a8a03746122
sharan669 <sharansurendra1995@gmail.com>
2019-08-29 15:47:41 -0400

    adding CSS and static content - Django - gradJourney

--- gradjourney/neuMSCS/views.py
+++ gradjourney/neuMSCS/views.py
@@ -4,4 +4,4 @@ from .models import Article
 # Create your views here.
 def neu_mscs_List(request):
     articles = Article.objects.all().order_by('date')
-    return render(request,'neuMSCS/neumscs_list.html', {'articles':articles});
+    return render(request,'neuMSCS/neumscs_list.html', {'articles':articles})
