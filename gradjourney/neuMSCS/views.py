dde58cfed9571ea0fed06c2023f11f60df7fbb36
sharan669 <sharansurendra1995@gmail.com>
2019-08-29 10:34:32 -0400

    Listing the articles dynamically in the article page - Django - gradJourney

--- gradjourney/neuMSCS/views.py
+++ gradjourney/neuMSCS/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def neu_mscs_List(request):
-    return render(request,'neuMSCS/neumscs_list.html');
+    articles = Article.objects.all().order_by('date')
+    return render(request,'neuMSCS/neumscs_list.html', {'articles':articles});
