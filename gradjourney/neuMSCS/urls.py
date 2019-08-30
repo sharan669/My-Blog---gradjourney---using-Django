b1cfbe9081e897fd65fc52780dd22e5184d36c29
sharan669 <sharansurendra1995@gmail.com>
2019-08-30 15:34:31 -0400

    Names URL's - Django - gradJourney blog

--- gradjourney/neuMSCS/urls.py
+++ gradjourney/neuMSCS/urls.py
@@ -2,9 +2,9 @@
 from django.conf.urls import url
 from . import views # since view is already in teh current directory so...
 
-
+app_name = 'neuMSCS'
 
 urlpatterns = [
-    url(r'^$',views.neu_mscs_List),
-    url(r'^(?P<slug>[\w-]+)/$', views.article_detail),
+    url(r'^$',views.neu_mscs_List, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
