ffc656d942893517ee6f6aa8324d6de069ebbc83
sharan669 <sharansurendra1995@gmail.com>
2019-08-30 21:32:08 -0400

    Adding media -  gradJourney - Django

--- gradjourney/gradjourney/urls.py
+++ gradjourney/gradjourney/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from . import views # since view is already in teh current directory so...
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -14,3 +15,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
